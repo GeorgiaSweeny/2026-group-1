6dc863fe7ad7d0c7ad05d17c9a4214dfd36f877d
Georgia Sweeny <124116920+GeorgiaSweeny@users.noreply.github.com>
2026-03-03 23:37:09 +0000

    added backgrounds tileset bg .jpg and .tsx
    .tsx tileset type "from collection of images"

---
+++ docs/assets/backgrounds/backgrounds.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="backgrounds" tilewidth="1472" tileheight="832" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="bg-atmosphere.jpg" width="365" height="261"/>
+ </tile>
+</tileset>
