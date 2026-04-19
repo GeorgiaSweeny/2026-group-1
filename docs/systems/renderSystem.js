fa0ff7bf9f301fda675dd5d905127781b86182de
Monal Gupta <monalmehal@gmail.com>
2026-04-19 20:51:12 +0100

    lil edit

--- docs/systems/renderSystem.js
+++ docs/systems/renderSystem.js
@@ -683,7 +683,7 @@ function renderInterpolate(oldState, newState, alpha){
             pop();
 
             // --- Screen space (fixed to viewport) --- //
-         //drawLighting(lightSources, cam, camScale);
+         drawLighting(lightSources, cam, camScale);
 
          // --- World space overlays (drawn above lighting) --- //
          push();
