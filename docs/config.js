a81e0195f9bc8d59313d6835d2f979d902fcf9ce
GeorgiaSweeny <georgia.sweeny@gmail.com>
2026-04-22 16:24:08 +0100

    add torch class and system small refactor, config now sets flicker speed and backout threshold, behavior modified flicker threshold and even more agressive blackout threshold

--- docs/config.js
+++ docs/config.js
@@ -160,7 +160,7 @@ export const PLAYER = {
 //======================
 export const POWER = {
   MAX_POWER: 100,
-  CURRENT_POWER: 100,
+  CURRENT_POWER: 16,
   LOW_POWER_THRESHOLD: 0.15,
   DRAIN_RATE: 0.5
   /* 
