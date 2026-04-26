d75ac8ca70a0869a9fb46887e563b85b368f2a19
Monal Gupta <monalmehal@gmail.com>
2026-04-26 20:23:30 +0100

    Collectible sound added - coin sound

--- docs/sketch.js
+++ docs/sketch.js
@@ -675,6 +675,7 @@ function setup() {
     () => roomSystem.getHazards(),
     () => pauseMenuSystem.getDifficulty(),
     () => enemySystem?.getEnemies() ?? [],
+    soundSystem,
   );
 
   enemySystem = createEnemySystem(
